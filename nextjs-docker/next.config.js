acefc67f4c1a47706bf49c356c23d0431cecb263
productuidev <productuidev@gmail.com>
2022-06-26 17:15:51 +0900

    docker container

--- nextjs-docker/next.config.js
+++ nextjs-docker/next.config.js
@@ -1,11 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
-
 module.exports = {
-  // ... rest of the configuration.
   output: 'standalone',
 }
