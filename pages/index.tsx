74c2f5b3e75e4f9e5bd0108c30ae6a31fd62e8bf
productuidev <productuidev@gmail.com>
2022-06-30 06:28:49 +0900

    testing-library

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,8 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <nav aria-label="fastcampus">fastcampus</nav>
+
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
