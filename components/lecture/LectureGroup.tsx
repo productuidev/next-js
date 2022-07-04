45c2f5f025fd9d9b2da75237522bafb1a06eccd6
productuidev <productuidev@gmail.com>
2022-07-05 05:35:45 +0900

    create components by test

---
+++ components/lecture/LectureGroup.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LectureGroup = (): JSX.Element => {
+  return (
+    <div>
+      <span>Best</span>
+      <h2 role="heading">강의제목</h2>
+    </div>
+  )
+}
+
+export default LectureGroup;
